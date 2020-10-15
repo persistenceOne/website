ecd530e557d6dc066bac8e350085df1d37eb0662
spy1236 <lucky1236@protonmail.com>
2020-10-15 16:05:16 +0530

    Modified UI changes

--- src/webpages/components/header.js
+++ src/webpages/components/header.js
@@ -39,15 +39,10 @@ class Header extends Component {
                             </button>
                             <div className={this.state.isOpen ? "collapse navbar-collapse show" : "collapse navbar-collapse"} style={{ display: this.state.isOpen ? "inline-grid" : "" }} id="navbarCollapse">
                                 <ul className="navbar-nav navbar-right team-link">
-                                    <li className="nav-item dropdown">
-                                        <NavDropdown title="About" id="basic-nav-dropdown">
-                                            <NavDropdown.Item><Link to="team">Team</Link></NavDropdown.Item>
-
-                                        </NavDropdown>
-                                    </li>
+                                    <li className="nav-item"><NavLink to="about" className="nav-link">About</NavLink></li>
                                     <li className="nav-item"><NavLink to="roadmap" className="nav-link">Roadmap</NavLink></li>
                                     <li className="nav-item"><a href="https://medium.com/persistence-blog" rel="noopener noreferrer" target="_blank" className="nav-link">Blog</a></li>
-                                    <li className="nav-item"><LangDropDown /></li>
+                                    {/* <li className="nav-item"><LangDropDown /></li> */}
                                 </ul>
                                 <div className={this.state.isOpen ? "nav-button" : "nav-button"}>
                                     <ul className="nav navbar-nav navbar-left">
