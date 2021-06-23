160cb727de12b2f3f3363490661a1511ea94f154
aditim121 <aditimaheshwari121@gmail.com>
2021-06-23 15:32:45 +0530

    functional compoennt

--- src/Containers/Stakedrop/RecordXPRTAddress.js
+++ src/Containers/Stakedrop/RecordXPRTAddress.js
@@ -1,53 +1,57 @@
 
-import React, { Component } from 'react'
+import React, {useEffect, useState} from 'react'
 import { Modal } from "react-bootstrap";
 import { NETWORK_ID } from "../../constants/config";
 import { getContractInstance } from "../../actions/utils";
 import { checkbech32 } from "../../actions/bech32Validation"
-export class RecordXPRTAddress extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            metamaskShow: false,
-            account: '',
-            disableBtn: '',
-        }
-        this.metamaskConnect = this.metamaskConnect.bind(this)
-        this.handlexprtAddressChange = this.handlexprtAddressChange.bind(this)
-    }
-    handlexprtAddressChange = (event) => {
-        
-       
-        let calAddress =  event.target.value
-      console.log(this.state.calAddress)
-      let checkBech32 = checkbech32(calAddress);
+
+const RecordXPRTAddress = () => {
+
+    const [metamaskShow, setMetamaskShow] = useState(false);
+    const [account, setAccount] = useState('');
+    const [disableBtn, setDisableBtn] = useState('');
+    const [errorxprtAddress, setErrorxprtAddress] = useState(false);
+    const [tcShow, setTcShow] = useState(false);
+    const [msgShowAlert, setMsgShowAlert] = useState(false);
+    const [history, setHistory] = useState([]);
+    const [calAddress, setCalAddress] = useState('');
+    const [xprtAddress, setXprtAddress] = useState('');
+    const [networkName, setNetworkName] = useState('');
+
+
+    const handlexprtAddressChange = (event) => {
+        event.preventDefault();
+
+        setCalAddress(event.target.xprtAddress.value)
+
+      let checkBech32 = checkbech32(event.target.xprtAddress.value);
       if (checkBech32) {
-          this.setState({ errorxprtAddress: false })
+          setErrorxprtAddress(false);
       } else {
-          this.setState({ errorxprtAddress: true })
+          setErrorxprtAddress(true);
       }
   }
-    rewardStakingAddress = async e => {
+    const rewardStakingAddress = async e => {
         e.preventDefault();
 
-        const calAddress = e.target.xprtAddress.value;
-        console.log(calAddress, 'calAddress')
-        console.log(this.props.address, ' this.props.address')
-        let checkBech32 = checkbech32(calAddress);
+        const _calAddress = e.target.xprtAddress.value;
+        setXprtAddress(_calAddress)
+        console.log(_calAddress, '_calAddress')
+        let checkBech32 = checkbech32(_calAddress);
         if (checkBech32) {
 
             const stakeDrop3 = await getContractInstance("StakeDrop3");
             await stakeDrop3.methods
-                .MagicTx(calAddress)
+                .MagicTx(_calAddress)
                 .send({
-                    from: this.state.account,
+                    from: account,
                     // gas: 100000,
                 })
                 .on("transactionHash", (receipt) => {
                     console.log(receipt, "  receipt");
                 })
                 .then((response) => {
-                    this.setState({ tcShow: true });
+                    setTcShow(true)
                     console.log(response, "completed");
                 })
                 .catch((e) => {
@@ -55,24 +59,24 @@ export class RecordXPRTAddress extends Component {
                 });
 
         } else {
-            this.setState({ errorxprtAddress: true })
+            setErrorxprtAddress(true);
         }
 
     }
-    handleCancelTerms = () => {
-        this.setState({ tcShow: false });
-        this.setState({ metamaskShow: false });
-        this.setState({ disableBtn: true });
-        this.props.history.push('/StakeDropMatic');
+    const handleCancelTerms = () => {
+        setTcShow(false)
+        setMetamaskShow(false);
+        setDisableBtn(true);
+        history.push('/StakeDropMatic');
     };
-    metamaskConnect = () => {
-        alert('coming')
+
+    const metamaskConnect = () => {
         const checkConnection = (cb) => {
             if (window.ethereum && window.ethereum.isMetaMask) {
                 cb(null)
             } else {
-                this.setState({ metamaskShow: true });
-                this.setState({ disableBtn: true });
+                setMetamaskShow(true)
+                setDisableBtn(true)
             }
         };
         checkConnection((err) => {
@@ -84,150 +88,143 @@ export class RecordXPRTAddress extends Component {
             window.ethereum.request({ method: 'eth_requestAccounts' })
                 .then((addr) => {
                     console.log(addr[0], 'address')
-                    this.setState({ account: addr[0] })
+                    setAccount(addr[0])
 
                 });
         });
     }
-    componentDidMount = () => {
+
+    useEffect(() => {
         window.ethereum.request({ method: 'eth_requestAccounts' })
             .then((addr) => {
                 console.log(addr[0], 'address')
-                this.setState({ account: addr[0] })
+                setAccount(addr[0])
             });
 
         window.ethereum.on('accountsChanged', (accounts) => {
             console.log(accounts[0])
-            this.setState({ account: accounts[0] })
+            setAccount(accounts[0])
         });
         window.ethereum.on('chainChanged', (chainId) => {
-            this.setState({ networkName: chainId })
+            setNetworkName(chainId)
             if (chainId === NETWORK_ID) {
-                this.setState({ msgShowAlert: true })
+                setMsgShowAlert(true)
             } else {
-                this.setState({ msgShowAlert: false })
+                setMsgShowAlert(false)
             }
         });
         window.ethereum.request({ method: 'eth_chainId' })
             .then((networkId) => {
-                this.setState({ networkName: networkId })
+                setNetworkName(networkId)
                 if (networkId === NETWORK_ID) {
-                    this.setState({ msgShowAlert: true })
+                    setMsgShowAlert(true)
                 } else {
-                    this.setState({ msgShowAlert: false })
+                    setMsgShowAlert(false)
                 }
 
             }).catch((error) => {
-                console.log(error)
-            })
-    }
-    render() {
-        return (
+            console.log(error)
+        })
 
-            <>
-                {this.props.maticstakeaddress ?
-                    <>
-                        {!this.state.msgShowAlert ?
-                            <h6 className="error valid-add">"Network ID mismatch. Select Ethereum Mainnet in your Metamask / Browser Wallet and Refresh Page"</h6>
-                            : <h6 className="error valid-add green">"Connected to Browser Wallet"</h6>}
-                    </>
-                    : null}
-                <div className="col-lg-12 matic-tutorial-section metmask-status">
-                    <p className="">Connect to Metamask or other Browser Wallet</p>
-                    <div className="btn-calculate mr-2">
-                        <button type="submit" disabled={this.state.disableBtn} onClick={this.metamaskConnect} className="btn">  {!this.props.maticstakeaddress ? <span>Connect</span> : <span>Connected</span>}</button>
-                    </div>
+        return () => {};
+    });
+
+    return (
+
+        <>
+            {account ?
+                <>
+                    {!msgShowAlert ?
+                        <h6 className="error valid-add">"Network ID mismatch. Select Ethereum Mainnet in your Metamask / Browser Wallet and Refresh Page"</h6>
+                        : <h6 className="error valid-add green">"Connected to Browser Wallet"</h6>}
+                </>
+                : null}
+            <div className="col-lg-12 matic-tutorial-section metmask-status">
+                <p className="">Connect to Metamask or other Browser Wallet</p>
+                <div className="btn-calculate mr-2">
+                    <button type="submit" disabled={disableBtn} onClick={metamaskConnect} className="btn">  {!account ? <span>Connect</span> : <span>Connected</span>}</button>
                 </div>
+            </div>
 
 
-                <div className="col-lg-12 stakerow">
-                    <div className="col-lg-12  header-section">
-                        <h5 className="heading-participate">Provide Persistence Address for Rewards</h5>
-                    </div>
-                    <div className="body-section">
-                        <h6 className="note-text mt-4"><span>Note:</span> Submit your Persistence Address (XPRT) and Staking Address (ETH) you used for MATIC Staking, to receive XPRT StakeDrop</h6>
-
-                        <form onSubmit={this.rewardStakingAddress}>
-                            <div className="inputstaking">
-                                <h5>Persistence Address</h5>
-                                <input
-                                    type="text"
-                                    name="xprtAddress"
-                                    id="xprtAddress"
-                                    value={this.state.xprtAddress}
-                                    onChange={this.handlexprtAddressChange}
-                                    placeholder="Enter Address"
-                                    required
-                                />
-
-                            </div>
-                            {this.state.errorxprtAddress ?
-                                <h6 className="text-left valid-add">Enter Valid Persistence Address</h6>
-                                :
-                                ""
-                            }
-
-
-                            <div className="inputstaking">
-                                <h5>Staking Address</h5>
-                                <input
-                                    type="text"
-                                    name="stakingAddress"
-                                    id="stakingAddress"
-                                    value={this.state.account}
-                                    onChange={this.handleAddressChange}
-                                    placeholder="--"
-                                    required
-                                    disabled
-                                />
-                            </div>
-
-                            <div className="inputstaking left-align-calculate">
-                                <h5>&nbsp;</h5>
-                                <div className="btn-calculate mr-2">
-                                    <button type="submit" disabled={!this.state.account || !this.state.msgShowAlert} className="btn">Submit</button>
-                                </div>
-
-                            </div>
-                            {this.state.errorAddress ?
-                                <h6 className="valid-add">Enter Valid Address</h6>
-                                :
-                                ""
-                            }
-                            {this.state.notParticipantAddress ?
-                                <h6 className="valid-add">Not a StakeDrop participant address, please send the magic transaction</h6>
-                                :
-                                ""
-                            }
-
-                        </form>
-
-
-
-
-
-                    </div>
-
+            <div className="col-lg-12 stakerow">
+                <div className="col-lg-12  header-section">
+                    <h5 className="heading-participate">Provide Persistence Address for Rewards</h5>
                 </div>
-                <Modal
-                    show={this.state.metamaskShow}
-                    onHide={this.handleClose}
-                    className="accountInfoModel"
-                    centered
-                >
-                    <Modal.Body>
-                        <p className="tc">Please install metamask
-                        </p>
-                        <div className="button-section">
+                <div className="body-section">
+                    <h6 className="note-text mt-4"><span>Note:</span> Submit your Persistence Address (XPRT) and Staking Address (ETH) you used for MATIC Staking, to receive XPRT StakeDrop</h6>
 
-                            <button className="btn decline" onClick={this.handleCancelTerms} >Ok</button>
+                    <form onSubmit={rewardStakingAddress}>
+                        <div className="inputstaking">
+                            <h5>Persistence Address</h5>
+                            <input
+                                type="text"
+                                name="xprtAddress"
+                                id="xprtAddress"
+                                value={xprtAddress}
+                                onChange={handlexprtAddressChange}
+                                placeholder="Enter Address"
+                                required
+                            />
+
                         </div>
-                    </Modal.Body>
-                </Modal>
-            </>
+                        {errorxprtAddress ?
+                            <h6 className="text-left valid-add">Enter Valid Persistence Address</h6>
+                            :
+                            ""
+                        }
 
-        )
-    }
+
+                        <div className="inputstaking">
+                            <h5>Staking Address</h5>
+                            <input
+                                type="text"
+                                name="stakingAddress"
+                                id="stakingAddress"
+                                value={account}
+                                //onChange={handleAddressChange}
+                                placeholder="--"
+                                required
+                                disabled
+                            />
+                        </div>
+
+                        <div className="inputstaking left-align-calculate">
+                            <h5>&nbsp;</h5>
+                            <div className="btn-calculate mr-2">
+                                <button type="submit" disabled={!account || !msgShowAlert} className="btn">Submit</button>
+                            </div>
+
+                        </div>
+
+                    </form>
+
+
+
+
+
+                </div>
+
+            </div>
+            <Modal
+                show={metamaskShow}
+                onHide={handleCancelTerms}
+                className="accountInfoModel"
+                centered
+            >
+                <Modal.Body>
+                    <p className="tc">Please install metamask
+                    </p>
+                    <div className="button-section">
+
+                        <button className="btn decline" onClick={handleCancelTerms} >Ok</button>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </>
+
+    )
+
 }
 
 export default RecordXPRTAddress
