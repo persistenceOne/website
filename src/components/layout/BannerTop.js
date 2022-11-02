835e1abc6eb936812f81b2fa6376be40050b56e0
blackpanther1881 <raju@persistence.one>
2022-11-02 20:00:56 +0400

    removed arrow

--- src/components/layout/BannerTop.js
+++ src/components/layout/BannerTop.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import Alert from "react-bootstrap/Alert";
-import rightarrow from "../../assets/images1/right-arrow.svg";
-import close from "../../assets/images1/close_icon.png";
+gimport close from "../../assets/images1/close_icon.png";
 
 
 const BannerTop = () => {
@@ -17,7 +16,6 @@ const BannerTop = () => {
                     <Alert className="nav-banner alert-dismissible">
                         <p>
                             Liquid staking solution for ATOM launching on 8th November
-                            <img src={rightarrow} className="ml-3" alt="arrow"/>
                         </p>
                         <img src={close} alt="close" className="close" onClick={closeBanner}/>
                     </Alert>
