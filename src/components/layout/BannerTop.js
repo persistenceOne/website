352d9867845cdbdb2060fa774f59ebd9c0f6ce01
blackpanther1881 <raju@persistence.one>
2022-11-02 22:03:59 +0400

    updating banner

--- src/components/layout/BannerTop.js
+++ src/components/layout/BannerTop.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import Alert from "react-bootstrap/Alert";
 import close from "../../assets/images1/close_icon.png";
-
+import stkATOM from "../../assets/images1/stkAtom.svg";
 
 const BannerTop = () => {
     const [banner, setBanner] = useState(true);
@@ -10,20 +10,19 @@ const BannerTop = () => {
         setBanner(false);
     }
     return (
+        banner ?
         <div className="container-fluid bannernav-section">
-            {banner ?
                 <div className="container">
-                    <Alert className="nav-banner alert-dismissible">
+                    <Alert className="nav-banner alert-dismissible d-flex align-items-center justify-content-center">
+                        <img src={stkATOM} alt={"stkATOM"} width={"26px"} className="pr-2"/>
                         <p>
-                            Liquid staking solution for ATOM launching on 8th November
+                            Liquid staking solution for ATOM launching on 8th November.
                         </p>
                         <img src={close} alt="close" className="close" onClick={closeBanner}/>
                     </Alert>
                 </div>
-                : null}
-
-
         </div>
+                : null
 
     );
 };
