7e81a7850a5f65e9ebc5f5c39df9856beb8dd565
blackpanther1881 <raju@persistence.one>
2023-01-26 07:07:28 +0400

    update ecosystem

--- src/Containers/Ecosystem/ecosystemContent.js
+++ src/Containers/Ecosystem/ecosystemContent.js
@@ -1,116 +1,137 @@
-import React, { useEffect, useState } from 'react';
-import { netwrokData } from '../Ecosystem/netwoks';
+import React, { useEffect, useState } from "react";
+import { netwrokData } from "../Ecosystem/netwoks";
 import Icon from "../../components/Icon";
 
 const EcosystemContent = () => {
-    const [availableNetwork, setAvailableNetwork] = useState();
-    const [active, setActive] = useState("1");
-    const handleClick = (event) => {
-        setActive(event.target.id);
-        if(event.target.id === '1'){
-            setAvailableNetwork(netwrokData);
-        }if(event.target.id === '2'){
-            setAvailableNetwork(netwrokData.filter(function(item){
-                return item.name === "dApp";
-            }));
-        }if(event.target.id === '3'){
-            setAvailableNetwork(netwrokData.filter(function(item){
-                return item.name === "Explorer";
-            }));
-        }if(event.target.id === '4'){
-            setAvailableNetwork(netwrokData.filter(function(item){
-                return item.name === "Wallet";
-            }));
-        }if(event.target.id === '5'){
-
-                setAvailableNetwork(netwrokData.filter(function(item){
-                    return item.name === "Defi";
-                }));
-
-        }if(event.target.id === '6'){
-            setAvailableNetwork(netwrokData.filter(function(item){
-                return item.name === "Tools";
-            }));
-
-        }
-
-
+  const [availableNetwork, setAvailableNetwork] = useState();
+  const [active, setActive] = useState("1");
+  const handleClick = (event) => {
+    setActive(event.target.id);
+    if (event.target.id === "1") {
+      setAvailableNetwork(netwrokData);
     }
-    useEffect(() => {
-        setAvailableNetwork(netwrokData);
+    if (event.target.id === "2") {
+      setAvailableNetwork(
+        netwrokData.filter(function (item) {
+          return item.name === "dApp";
+        })
+      );
+    }
+    if (event.target.id === "3") {
+      setAvailableNetwork(
+        netwrokData.filter(function (item) {
+          return item.name === "Explorer";
+        })
+      );
+    }
+    if (event.target.id === "4") {
+      setAvailableNetwork(
+        netwrokData.filter(function (item) {
+          return item.name === "Wallet";
+        })
+      );
+    }
+    if (event.target.id === "5") {
+      setAvailableNetwork(
+        netwrokData.filter(function (item) {
+          return item.name === "Defi";
+        })
+      );
+    }
+    if (event.target.id === "6") {
+      setAvailableNetwork(
+        netwrokData.filter(function (item) {
+          return item.name === "Tools";
+        })
+      );
+    }
+  };
+  useEffect(() => {
+    setAvailableNetwork(netwrokData);
+  }, []);
 
-
-    }, []);
-
-    return (
+  return (
     <section className="home-ecosystem-section ecosystem-section ecos">
-        <div className="container">
-            <h3 className="section-title">Persistence Ecosystem</h3>
-            <p className="sub-heading">Discover the suite of applications, wallets, explorers and tools within the ecosystem
-            </p>
-            <div className={'sortbuttons'} id={'myDIV'}>
-            <button
-                key={1}
-                className={active === "1" ? "active" : undefined}
-                id={"1"}
-                onClick={handleClick}>
-                All</button>
-            <button
-                key={2}
-                className={active === "2" ? "active" : undefined}
-                id={"2"}
-                onClick={handleClick}
-            >DAPPS</button>
-            <button
-                key={3}
-                className={active === "3" ? "active" : undefined}
-                id={"3"}
-                onClick={handleClick}
-            >EXPLORER</button>
-            <button
-                key={4}
-                className={active === "4" ? "active" : undefined}
-                id={"4"}
-                onClick={handleClick}
-            >WALLETS</button>
-            <button
-                key={5}
-                className={active === "5" ? "active" : undefined}
-                id={"5"}
-                onClick={handleClick}
-            >DEFI</button>
-            <button
-                key={6}
-                className={active === "6" ? "active" : undefined}
-                id={"6"}
-                onClick={handleClick}
-            >TOOLS</button>
-
-            </div>
-            <div className="network-section">
-                {availableNetwork && availableNetwork.map((item, i) => (
-                    <a href={item.wikilink} target="_blank" rel="noopener noreferrer">
-                        <div key={i} className="networklist-tiles">
-
-                            <Icon viewClass="social_icon_imgg" icon={item.icon} />
-                            <h5>{item.network}</h5>
-                            <h6>{item.description}</h6>
-<span className={'networkname'}>{item.name}</span>
-
-<span className={'status'}>
-     {
-         item.status==='ComingSoon' ?
-             <span className={'nodot'}></span> : <span className={'dot'}></span>
-     }
-    &nbsp;{item.status}</span>
-                        </div> </a>
-                ))}
-
-            </div>
-
+      <div className="container">
+        <h3 className="section-title">Persistence Ecosystem</h3>
+        <p className="sub-heading">
+          Discover the suite of applications, wallets, explorers and tools
+          within the ecosystem
+        </p>
+        <div className={"sortbuttons"} id={"myDIV"}>
+          <button
+            key={1}
+            className={active === "1" ? "active" : undefined}
+            id={"1"}
+            onClick={handleClick}
+          >
+            All
+          </button>
+          <button
+            key={2}
+            className={active === "2" ? "active" : undefined}
+            id={"2"}
+            onClick={handleClick}
+          >
+            DAPPS
+          </button>
+          <button
+            key={3}
+            className={active === "3" ? "active" : undefined}
+            id={"3"}
+            onClick={handleClick}
+          >
+            EXPLORER
+          </button>
+          <button
+            key={4}
+            className={active === "4" ? "active" : undefined}
+            id={"4"}
+            onClick={handleClick}
+          >
+            WALLETS
+          </button>
+          <button
+            key={5}
+            className={active === "5" ? "active" : undefined}
+            id={"5"}
+            onClick={handleClick}
+          >
+            DEFI
+          </button>
+          <button
+            key={6}
+            className={active === "6" ? "active" : undefined}
+            id={"6"}
+            onClick={handleClick}
+          >
+            TOOLS
+          </button>
         </div>
-    </section>
+        <div className="network-section">
+          {availableNetwork &&
+            availableNetwork.map((item, i) => (
+              <a href={item.wikilink} target="_blank" rel="noopener noreferrer">
+                <div key={i} className="networklist-tiles">
+                  <Icon viewClass="social_icon_imgg" icon={item.icon} />
+                  <h5>{item.network}</h5>
+                  <h6>{item.description}</h6>
+                  <span className={"networkname"}>{item.name}</span>
 
-)
-}
+                  <span className={"status"}>
+                    {item.status === "ComingSoon" ? (
+                      <span className={"nodot"}></span>
+                    ) : (
+                      <span className={"dot"}></span>
+                    )}
+                    &nbsp;{item.status}
+                  </span>
+                </div>{" "}
+              </a>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 export default EcosystemContent;
--- src/Containers/Ecosystem/netwoks.js
+++ src/Containers/Ecosystem/netwoks.js
@@ -1,214 +1,220 @@
 export const netwrokData = [
-    {
-        network: "pSTAKE",
-        description: "Liquid Staking protocol",
-        icon: "pstake-icon",
-        name: "dApp",
-        status: "Live",
-        wikilink: "https://pstake.finance/"
-    },
-    {
-        network: "pWallet",
-        description: "Persistence Ecosystem Wallet",
-        icon: "xprt",
-        name: "Wallet",
-        status: "Live",
-        wikilink: "https://wallet.persistence.one/"
-    },
-    {
-        network: "Bamboo",
-        description: "Lending Protocol For Yield-Generating Assets",
-        icon: "bamboo",
-        name: "dApp",
-        status: "Coming soon",
-        wikilink: "https://bamboo.zone/"
-    },
-    {
-        network: "Dexter",
-        description: "Interchain DEX for Yield-Generating Assets",
-        icon: "dexter",
-        name: "dApp",
-        status: "Coming Soon",
-        wikilink: "https://dexter.zone/"
-    },
-    {
-        network: "Persistence Bridge",
-        description: "Ethereum <> Persistence Bridge Interface",
-        icon: "xprt",
-        name: "Tools",
-        status: "Live",
-        wikilink: "https://bridge.persistence.one/"
-    },
-    {
-        network: "Osmosis",
-        description: "Interchain Liquidity Lab",
-        icon: "osmosis",
-        name: "Defi",
-        status: "Live",
-        wikilink: "https://info.osmosis.zone/token/XPRT"
-    },
-    {
-        network: "Junoswap",
-        description: "Decentralised Exchange",
-        icon: "junoswap",
-        name: "Defi",
-        status: "Live",
-        wikilink: "https://junoswap.com/pools/JUNO-XPRT"
-    },
-    {
-        network: "Sifchain",
-        description: "Decentralised Exchange",
-        icon: "sifchain",
-        name: "Defi",
-        status: "Live",
-        wikilink: "https://sifchain.network/"
-    },
-    {
-        network: "Keplr",
-        description: "Cosmos Ecosystem wallet",
-        icon: "keplr",
-        name: "Wallet",
-        status: "Live",
-        wikilink: "https://wallet.keplr.app/#/core/stake"
-    },
-    {
-        network: "Cosmostation",
-        description: "Cosmos Ecosystem wallet",
-        icon: "cosmostation",
-        name: "Wallet",
-        status: "Live",
-        wikilink: "https://www.cosmostation.io/"
-    },
-    {
-        network: "Coin98",
-        description: "All-in-One DeFi platform",
-        icon: "coin98",
-        name: "Wallet",
-        status: "Live",
-        wikilink: "https://coin98.com"
-    },
-    {
-        network: "Leap",
-        description: "non-custodial super wallet for web3",
-        icon: "leap",
-        name: "Wallet",
-        status: "Live",
-        wikilink: "https://www.leapwallet.io/"
-    },
-    {
-        network: "Ledger",
-        description: "Hardware Wallet",
-        icon: "ledger",
-        name: "Wallet",
-        status: "Live",
-        wikilink: "https://www.ledger.com"
-    },
-    {
-        network: "Steakwallet",
-        description: "Mobile Wallet",
-        icon: "steakwallet",
-        name: "Wallet",
-        status: "Live",
-        wikilink: "https://steakwallet.fi/"
-    },
-    {
-        network: "Mintscan",
-        description: "Cosmos Ecosystem Explorer",
-        icon: "mintscan",
-        name: "Explorer",
-        status: "Live",
-        wikilink: "https://www.mintscan.io/persistence"
-    },
-    {
-        network: "Ping.pub",
-        description: "Cosmos Ecosystem Explorer",
-        icon: "pingpub",
-        name: "Explorer",
-        status: "Live",
-        wikilink: "https://ping.pub/persistence"
-    },
-    {
-        network: "Atomscan",
-        description: "Cosmos Ecosystem Explorer",
-        icon: "atomscan",
-        name: "Explorer",
-        status: "Live",
-        wikilink: "https://atomscan.com/persistence"
-    },
-    {
-        network: "SmartStake",
-        description: "Performance assessment & analytics dashboard",
-        icon: "smartstake",
-        name: "Tools",
-        status: "Live",
-        wikilink: "https://xprt.smartstake.io/"
-    },
-    {
-        network: "Injective Protocol",
-        description: "Smart Contracts platform optimised for DeFi",
-        icon: "inj",
-        name: "Defi",
-        status: "Live",
-        wikilink: "https://injective.exchange/spot/xprt-usdt"
-    },
-    {
-        network: "SushiSwap",
-        description: "Decentralised Exchange",
-        icon: "sushiswap",
-        name: "Defi",
-        status: "Live",
-        wikilink: "https://analytics.sushi.com/tokens/0x44017598f2af1bd733f9d87b5017b4e7c1b28dde"
-    },
-    {
-        network: "Smart Nodes",
-        description: "Cosmos Ecosystem Explorer",
-        icon: "smartnode",
-        name: "Explorer",
-        status: "Live",
-        wikilink: "https://smartnodes.one/dashboard/persistence"
-    },
-    {
-        network: "Citadel One",
-        description: "Non-custodial wallet",
-        icon: "citadelone",
-        name: "Wallet",
-        status: "Live",
-        wikilink: "https://app.citadel.one/"
-    },
+  {
+    network: "pSTAKE",
+    description: "Liquid Staking protocol",
+    icon: "pstake-icon",
+    name: "dApp",
+    status: "Live",
+    wikilink: "https://pstake.finance/"
+  },
+  {
+    network: "pWallet",
+    description: "Persistence Ecosystem Wallet",
+    icon: "xprt",
+    name: "Wallet",
+    status: "Live",
+    wikilink: "https://wallet.persistence.one/"
+  },
+  {
+    network: "Bamboo",
+    description: "Lending Protocol For Yield-Generating Assets",
+    icon: "bamboo",
+    name: "dApp",
+    status: "Coming soon",
+    wikilink: "https://bamboo.zone/"
+  },
+  {
+    network: "Dexter",
+    description: "Interchain DEX for Yield-Generating Assets",
+    icon: "dexter",
+    name: "dApp",
+    status: "Coming Soon",
+    wikilink: "https://dexter.zone/"
+  },
+  {
+    network: "Persistence Bridge",
+    description: "Ethereum <> Persistence Bridge Interface",
+    icon: "xprt",
+    name: "Tools",
+    status: "Live",
+    wikilink: "https://bridge.persistence.one/"
+  },
+  {
+    network: "Osmosis",
+    description: "Interchain Liquidity Lab",
+    icon: "osmosis",
+    name: "Defi",
+    status: "Live",
+    wikilink: "https://info.osmosis.zone/token/XPRT"
+  },
+  {
+    network: "Junoswap",
+    description: "Decentralised Exchange",
+    icon: "junoswap",
+    name: "Defi",
+    status: "Live",
+    wikilink: "https://junoswap.com/pools/JUNO-XPRT"
+  },
+  {
+    network: "Sifchain",
+    description: "Decentralised Exchange",
+    icon: "sifchain",
+    name: "Defi",
+    status: "Live",
+    wikilink: "https://sifchain.network/"
+  },
+  {
+    network: "Keplr",
+    description: "Cosmos Ecosystem wallet",
+    icon: "keplr",
+    name: "Wallet",
+    status: "Live",
+    wikilink: "https://wallet.keplr.app/#/core/stake"
+  },
+  {
+    network: "Cosmostation",
+    description: "Cosmos Ecosystem wallet",
+    icon: "cosmostation",
+    name: "Wallet",
+    status: "Live",
+    wikilink: "https://www.cosmostation.io/"
+  },
+  {
+    network: "Coin98",
+    description: "All-in-One DeFi platform",
+    icon: "coin98",
+    name: "Wallet",
+    status: "Live",
+    wikilink: "https://coin98.com"
+  },
+  {
+    network: "Leap",
+    description: "non-custodial super wallet for web3",
+    icon: "leap",
+    name: "Wallet",
+    status: "Live",
+    wikilink: "https://www.leapwallet.io/"
+  },
+  {
+    network: "Ledger",
+    description: "Hardware Wallet",
+    icon: "ledger",
+    name: "Wallet",
+    status: "Live",
+    wikilink: "https://www.ledger.com"
+  },
+  {
+    network: "Steakwallet",
+    description: "Mobile Wallet",
+    icon: "steakwallet",
+    name: "Wallet",
+    status: "Live",
+    wikilink: "https://steakwallet.fi/"
+  },
+  {
+    network: "Mintscan",
+    description: "Cosmos Ecosystem Explorer",
+    icon: "mintscan",
+    name: "Explorer",
+    status: "Live",
+    wikilink: "https://www.mintscan.io/persistence"
+  },
+  {
+    network: "Ping.pub",
+    description: "Cosmos Ecosystem Explorer",
+    icon: "pingpub",
+    name: "Explorer",
+    status: "Live",
+    wikilink: "https://ping.pub/persistence"
+  },
+  {
+    network: "Atomscan",
+    description: "Cosmos Ecosystem Explorer",
+    icon: "atomscan",
+    name: "Explorer",
+    status: "Live",
+    wikilink: "https://atomscan.com/persistence"
+  },
+  {
+    network: "SmartStake",
+    description: "Performance assessment & analytics dashboard",
+    icon: "smartstake",
+    name: "Tools",
+    status: "Live",
+    wikilink: "https://xprt.smartstake.io/"
+  },
+  {
+    network: "Injective Protocol",
+    description: "Smart Contracts platform optimised for DeFi",
+    icon: "inj",
+    name: "Defi",
+    status: "Live",
+    wikilink: "https://injective.exchange/spot/xprt-usdt"
+  },
+  {
+    network: "SushiSwap",
+    description: "Decentralised Exchange",
+    icon: "sushiswap",
+    name: "Defi",
+    status: "Live",
+    wikilink:
+      "https://analytics.sushi.com/tokens/0x44017598f2af1bd733f9d87b5017b4e7c1b28dde"
+  },
+  {
+    network: "Smart Nodes",
+    description: "Cosmos Ecosystem Explorer",
+    icon: "smartnode",
+    name: "Explorer",
+    status: "Live",
+    wikilink: "https://smartnodes.one/dashboard/persistence"
+  },
+  {
+    network: "Citadel One",
+    description: "Non-custodial wallet",
+    icon: "citadelone",
+    name: "Wallet",
+    status: "Live",
+    wikilink: "https://app.citadel.one/"
+  },
 
-    {
-        network: "Gravity Bridge",
-        description: "Trustless bridge between Ethereum and the Cosmos ecosystem",
-        icon: "gravitybridge",
-        name: "Tools",
-        status: "Live",
-        wikilink: "https://spacestation.zone/"
-    },
-    {
-        network: "Dexmos.app",
-        description: "Osmosis Analytics",
-        icon: "dexmosapp",
-        name: "Tools",
-        status: "Live",
-        wikilink: "https://www.dexmos.app/"
-    },
-    {
-        network: "Obervatory",
-        description: "Chain Analytics",
-        icon: "observe",
-        name: "Tools",
-        status: "Live",
-        wikilink: "https://observatory.zone/persistence"
-    },
-    {
-        network: "Pulsar",
-        description: "Asset Tracking Application",
-        icon: "pulsar",
-        name: "Tools",
-        status: "Live",
-        wikilink: "https://app.pulsar.finance/portfolio"
-    },
-
-]
-
-
+  {
+    network: "Gravity Bridge",
+    description: "Trustless bridge between Ethereum and the Cosmos ecosystem",
+    icon: "gravitybridge",
+    name: "Tools",
+    status: "Live",
+    wikilink: "https://spacestation.zone/"
+  },
+  {
+    network: "Dexmos.app",
+    description: "Osmosis Analytics",
+    icon: "dexmosapp",
+    name: "Tools",
+    status: "Live",
+    wikilink: "https://www.dexmos.app/"
+  },
+  {
+    network: "Obervatory",
+    description: "Chain Analytics",
+    icon: "observe",
+    name: "Tools",
+    status: "Live",
+    wikilink: "https://observatory.zone/persistence"
+  },
+  {
+    network: "Pulsar",
+    description: "Asset Tracking Application",
+    icon: "pulsar",
+    name: "Tools",
+    status: "Live",
+    wikilink: "https://app.pulsar.finance/portfolio"
+  },
+  {
+    network: "Cosmosdash",
+    description: "XPRT and Osmosis Pools Analytics",
+    icon: "cosmosdash",
+    name: "Tools",
+    status: "Live",
+    wikilink: "https://www.cosmodash.zone/"
+  }
+];
