e424acb954171293d017b1397002e5164a654b3b
blackpanther1881 <raju@persistence.one>
2023-05-15 14:56:20 +0530

    update chain tvl

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,9 +7,10 @@ export const fetchChainTVL = async () => {
         const response = await axios.get(defillamaApi)
         console.log(response, "Response")
         if (response && response.data && response.data.protocols){
-            const protocol = response.data.protocols.find((item)=> item.name === 'Dexter');
+            const protocol = response.data.protocols.find((item)=> item.category === 'Dexes' && item.name === 'Dexter');
+            const liquidStake = response.data.protocols.find((item)=> item.category === 'Liquid Staking' && item.name === 'pSTAKE Finance');
             if (protocol){
-                return (protocol.chainTvls.Persistence.tvl).toFixed();
+                return (Number(protocol.chainTvls.Persistence.tvl)+Number(liquidStake.chainTvls.Persistence.tvl)).toFixed();
             }
         }
         return 0
