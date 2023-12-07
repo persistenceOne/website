a35ff9bf8cbaa42fdfcde76f54bf14af74a8d654
blackpanther1881 <raju@persistence.one>
2023-12-07 16:39:56 +0530

    update top banner

--- src/components/layout/BannerTop.js
+++ src/components/layout/BannerTop.js
@@ -1,44 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Alert from "react-bootstrap/Alert";
-import stkATOM from "../../assets/images1/stkAtom.svg";
+import stkOsmo from "../../assets/images1/stkOsmo.svg";
 import Icon from "../Icon";
 
 const BannerTop = () => {
-    const [banner, setBanner] = useState(true);
+  const [banner, setBanner] = useState(true);
 
-    const closeBanner = () => {
-        setBanner(false);
-    }
+  const closeBanner = () => {
+    setBanner(false);
+  };
 
-    return (
-        banner ?
-        <div className="container-fluid bannernav-section">
-                <div className="container">
-                    <Alert className="nav-banner alert-dismissible d-flex align-items-center justify-content-center">
-                        <img src={stkATOM} alt={"stkATOM"} className="pr-2 logo"/>
-                        <p>
-                            LSM support now live on pSTAKE. Liquid stake your already staked ATOM now.&nbsp;
-                            <a
-                                className="link"
-                                href={'https://cosmos.pstake.finance/'}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <b>here!</b>
-                            </a>
-                        </p>
-                        <div onClick={closeBanner} className="close">
-                        <Icon
-                            viewClass="close fill-[#fff] absolute top-[13px] right-[20px] cursor-pointer"
-                            icon="close"
-                        />
-                        </div>
-                    </Alert>
-                </div>
-        </div>
-                : null
-
-    );
+  return banner ? (
+    <div className="container-fluid bannernav-section">
+      <div className="container">
+        <Alert className="nav-banner alert-dismissible d-flex align-items-center justify-content-center">
+          <img src={stkOsmo} alt={"stkOsmo"} className="pr-2 logo" />
+          <p>
+            stkOsmo is LIVE on pSTAKE. Liquid Stake your OSMO with 75
+            validators&nbsp;
+            <a
+              className="link"
+              href={
+                "https://app.pstake.finance/cosmos?token=OSMO&chain=osmosis"
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <b>now</b>
+            </a>
+          </p>
+          <div onClick={closeBanner} className="close">
+            <Icon
+              viewClass="close fill-[#fff] absolute top-[13px] right-[20px] cursor-pointer"
+              icon="close"
+            />
+          </div>
+        </Alert>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default BannerTop;
