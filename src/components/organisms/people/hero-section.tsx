c5962b8999daa6abeaffd991235f9c764b9a582c
blackpanther1881 <rj.vemula81@gmail.com>
2024-02-22 16:02:08 +0530

    sanity fixes

--- src/components/organisms/people/hero-section.tsx
+++ src/components/organisms/people/hero-section.tsx
@@ -6,7 +6,7 @@ import {
   VStack,
   Heading,
   HStack,
-  Button,
+  Box,
   Text
 } from "@chakra-ui/react";
 import Image from "next/image";
@@ -16,22 +16,24 @@ const HeroSection = () => {
   return (
     <Container maxW={"1440px"} px={"40px"}>
       <Spacer h={20} />
-      <Flex justify={"space-between"} align={"flex-start"} h="70vh">
-        <VStack align={"stretch"} alignSelf={"flex-end"}>
-          <Heading variant={"main"}>
+      <Flex justify={"space-between"} align={"flex-start"} pos={"relative"}>
+        <VStack mt={"250px"}>
+          <Heading variant={"main"} color={"text.blackHigh"}>
             Humility. Integrity. <br /> Patience. Persistence.
           </Heading>
           <Text textAlign="left" color="#3D3D3DB2">
             Learn more about the people behind Persistence One, what it means to
-            be a Persister, and how to become one.
+            be a<br /> Persister, and how to become one.
           </Text>
         </VStack>
-        <Image
-          src="/images/xprt-page/persistence-coin.svg"
-          alt="Persistence Coin"
-          width={400}
-          height={400}
-        />
+        <Box pos={"absolute"} right={"0"}>
+          <Image
+            src="/images/xprt-page/persistence-coin.svg"
+            alt="Persistence Coin"
+            width={400}
+            height={400}
+          />
+        </Box>
       </Flex>
     </Container>
   );
