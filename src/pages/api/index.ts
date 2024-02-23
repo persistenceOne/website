49b1b3032924fa781cab3a318e33bc9ff38f7d40
blackpanther1881 <rj.vemula81@gmail.com>
2024-02-23 16:08:18 +0530

    update api fetching

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -14,6 +14,8 @@ export const DEXTER_POOL_URL = "https://api.core-1.dexter.zone/v1/graphql";
 export const OSMO_APR_URL =
   "https://public-osmosis-api.numia.xyz/pools_apr?pool=15";
 export const OSMO_TVL_URL = "https://api-osmosis.imperator.co/pools/v2/15";
+export const MarketCap_API =
+  "https://api.coingecko.com/api/v3/coins/persistence";
 
 export const fetchChainTVL = async () => {
   try {
@@ -255,5 +257,17 @@ export const fetchOsmosisPoolInfo = async () => {
   } catch (e) {
     console.log(e, "error in fetchOsmoPool2Info");
     return osmoInfo;
+  }
+};
+
+export const getMarketCap = async () => {
+  try {
+    const res = await axios.get(MarketCap_API);
+    if (res && res.data) {
+      return Number(res.data.market_data?.market_cap.usd);
+    }
+    return 0;
+  } catch (e) {
+    return 0;
   }
 };
