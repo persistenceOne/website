6e9ba417db2db24f2f2473db780d135b55957233
rajuvemula <rj.vemula81@gmail.com>
2024-07-19 23:31:43 +0530

    update footer

--- src/components/molecules/footer.tsx
+++ src/components/molecules/footer.tsx
@@ -58,7 +58,7 @@ const footerSocialLinks = [
     link: "https://www.mintscan.io/persistence/",
     icon: "mintscan",
     iconType: "stroke",
-    color: "#9C9C9C"
+    color: "#918678"
   }
 ];
 
@@ -82,53 +82,43 @@ const footerLinks = [
         isExternal: true
       },
       {
-        title: "Documentation",
+        title: "Docs",
+        link: "https://docs.persistence.one/",
+        isExternal: true
+      }
+    ]
+  },
+  {
+    title: "XPRT",
+    links: [
+      {
+        title: "About",
+        link: "/xprt",
+        isExternal: false
+      },
+      {
+        title: "XPRT Staking",
+        link: "/tech",
+        isExternal: true
+      },
+      {
+        title: "pWallet",
+        link: "",
+        isExternal: true
+      },
+      {
+        title: "Explorers",
         link: "https://docs.persistence.one",
         isExternal: true
       }
     ]
   },
   {
-    title: "Network",
-    links: [
-      {
-        title: "XPRT",
-        link: "/xprt",
-        isExternal: false
-      },
-      {
-        title: "Tech",
-        link: "/tech",
-        isExternal: false
-      }
-    ]
-  },
-  {
-    title: "Products",
-    links: [
-      {
-        title: "Persistence DEX",
-        link: "https://app.persistence.one",
-        isExternal: true
-      },
-      {
-        title: "XPRT Staking",
-        link: "https://wallet.keplr.app/chains/persistence",
-        isExternal: true
-      },
-      {
-        title: "pWallet",
-        link: "https://wallet.persistence.one",
-        isExternal: true
-      }
-    ]
-  },
-  {
-    title: "Community",
+    title: "Connect",
     links: [
       {
         title: "People",
-        link: "/values",
+        link: "/people",
         isExternal: false
       },
       {
@@ -137,7 +127,7 @@ const footerLinks = [
         isExternal: true
       },
       {
-        title: "Community Forum",
+        title: "Forum",
         link: "https://forum.persistence.one",
         isExternal: true
       }
@@ -153,77 +143,94 @@ const Footer = () => {
         px={{ base: "12px", md: "30px" }}
         py={{ base: "30px", md: "60px" }}
       >
-        <HStack
-          alignItems={"center"}
-          justifyContent={"space-between"}
-          py={{ base: "10px", md: "16px" }}
-        >
-          <Image
-            src={"images/persistence-logo.svg"}
-            width={186}
-            height={24}
-            alt={"Persistence Logo"}
-          />
-          <HStack gap={4} display={{ base: "none", md: "flex" }}>
-            {footerSocialLinks.map((link) => (
-              <Link
-                href={link.link}
-                key={link.name}
-                target="_blank"
-                rel="noopenner noreferrer"
-              >
-                {link.icon !== "github" ? (
-                  <Box
-                    w={"24px"}
-                    h={"24px"}
-                    _hover={{ bg: "white" }}
-                    borderRadius={"100%"}
-                    className={"icon-box"}
-                    bg={"#9C9C9C"}
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center"
-                    }}
-                  >
-                    <Icon
-                      icon={link.icon}
-                      color="white"
-                      viewClass={`footer-icon ${link.iconType}`}
-                    />
-                  </Box>
-                ) : (
-                  <Box
-                    w={"24px"}
-                    h={"24px"}
-                    _hover={{ bg: "white" }}
-                    borderRadius={"100%"}
-                    className={"icon-box"}
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center"
-                    }}
-                  >
-                    <Icon
-                      icon={link.icon}
-                      color="white"
-                      viewClass={` ${link.iconType}`}
-                    />
-                  </Box>
-                )}
-              </Link>
-            ))}
-          </HStack>
-        </HStack>
-        <Divider mt={"16px"} mb={"32px"} borderColor={"#FCFCFC33"} />
         <Flex
           display={{ base: "block", md: "flex" }}
-          justifyContent={"start"}
+          justify={"space-between"}
           align={"flex-start"}
-          color={"#918678"}
-          mb={"60px"}
+          color={"#ffffff"}
         >
+          <VStack
+            align={"flex-start"}
+            mb={{ base: "20px", md: "0" }}
+            w={{ base: "100%", md: "30%" }}
+          >
+            <Image
+              src={"images/persistence-logo.svg"}
+              width={186}
+              height={24}
+              alt={"Persistence Logo"}
+            />
+            <Link
+              href={
+                "https://persistence.notion.site/Persistence-Media-Kit-50e8f0eacab1466ebe7899d3773a48a3"
+              }
+              target={"_blank"}
+            >
+              <Button
+                variant={"link"}
+                rightIcon={<ArrowForwardIcon />}
+                color={"#ECECEC"}
+                fontWeight={600}
+                fontSize={16}
+                _hover={{
+                  color: "primary.red"
+                }}
+              >
+                Download Media Kit
+              </Button>
+            </Link>
+            <HStack gap={4} mt={4} display={{ base: "none", md: "flex" }}>
+              {footerSocialLinks.map((link) => (
+                <Link
+                  href={link.link}
+                  key={link.name}
+                  target="_blank"
+                  rel="noopenner noreferrer"
+                >
+                  {link.icon !== "github" ? (
+                    <Box
+                      w={"24px"}
+                      h={"24px"}
+                      _hover={{ bg: "white" }}
+                      borderRadius={"100%"}
+                      className={"icon-box"}
+                      bg={"#918678"}
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"
+                      }}
+                    >
+                      <Icon
+                        icon={link.icon}
+                        color="white"
+                        viewClass={`footer-icon ${link.iconType}`}
+                      />
+                    </Box>
+                  ) : (
+                    <Box
+                      w={"24px"}
+                      h={"24px"}
+                      _hover={{ bg: "white" }}
+                      borderRadius={"100%"}
+                      className={"icon-box"}
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"
+                      }}
+                    >
+                      <Icon
+                        icon={link.icon}
+                        color="white"
+                        viewClass={` ${link.iconType}`}
+                      />
+                    </Box>
+                  )}
+                </Link>
+              ))}
+            </HStack>
+          </VStack>
           {/*<Stack*/}
           {/*  gap={16}*/}
           {/*  align={"flex-start"}*/}
@@ -232,17 +239,13 @@ const Footer = () => {
           <SimpleGrid
             flex={1}
             columns={2}
-            gap={"120px"}
-            justifyContent={"start"}
+            gap={4}
+            justifyContent={"space-between"}
             display={{ base: "grid", md: "flex" }}
           >
             {footerLinks.map((link) => (
               <VStack key={link.title} align={"flex-start"} gap={4}>
-                <Heading
-                  variant={"footerHeading"}
-                  fontSize={"20px"}
-                  color={"#918678"}
-                >
+                <Heading color={"#918678"} fontSize={"20px"}>
                   {link.title}
                 </Heading>
                 {link.links.map((item) => (
@@ -287,7 +290,7 @@ const Footer = () => {
                 h={"24px"}
                 borderRadius={"100%"}
                 className={"icon-box"}
-                bg={"#9C9C9C"}
+                bg={"#918678"}
                 style={{
                   display: "flex",
                   justifyContent: "center",
@@ -304,7 +307,7 @@ const Footer = () => {
           ))}
         </HStack>
         <Box display={{ base: "block", md: "block" }}>
-          {/*<Divider mt={"28px"} borderColor={"#00000033"} />*/}
+          <Divider mt={"28px"} borderColor={"#ECECEC33"} />
           <Flex
             flexDir={{ base: "column", md: "row" }}
             justify={"space-between"}
@@ -312,14 +315,14 @@ const Footer = () => {
             mt={4}
             gap={4}
           >
-            <Text fontSize={14} fontWeight={400} color={"#FCFCFCE5"}>
+            <Text fontSize={14} fontWeight={400} color={"#ffffff"}>
               © Copyright 2024 Persistence Labs. All rights reserved.
             </Text>
             <HStack gap={8}>
               <Link href={"/termsofuse"}>
                 <Button
                   variant={"link"}
-                  color={"#FCFCFCE5"}
+                  color={"#ffffff"}
                   fontSize={14}
                   fontWeight={400}
                 >
@@ -329,7 +332,7 @@ const Footer = () => {
               <Link href={"/privacy"}>
                 <Button
                   variant={"link"}
-                  color={"#FCFCFCE5"}
+                  color={"#ffffff"}
                   fontSize={14}
                   fontWeight={400}
                 >
