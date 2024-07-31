464e2fb14a905804dff89e8abfdfd3b65ce812a3
rajuvemula <rj.vemula81@gmail.com>
2024-07-31 18:07:58 +0530

    text updates

--- src/components/organisms/xprt/hero-section.tsx
+++ src/components/organisms/xprt/hero-section.tsx
@@ -71,7 +71,7 @@ const HeroSection = () => {
             mb={{ base: "20px", "2xl": "20px" }}
           >
             <Heading
-              maxW={{ base: "400px", xl: "600px" }}
+              maxW={{ base: "400px", xl: "650px" }}
               fontSize={{ base: "36px", xl: "44px" }}
               lineHeight={{ base: "50px", xl: "64px" }}
               color={"#633C0D"}
