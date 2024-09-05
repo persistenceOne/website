ccbac87fa319c52c8ccaae72195144c8e36d0435
rajuvemula <rj.vemula81@gmail.com>
2024-09-05 11:18:40 +0530

    add dynamic loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
-import BlogSection from "@/components/organisms/homepage/blog-section";
-import DappsSection from "@/components/organisms/homepage/dapps-section";
-import HeroSection from "@/components/organisms/homepage/hero-section";
-import Overview from "@/components/organisms/homepage/overview";
 import { Box, Spacer } from "@chakra-ui/react";
 import React from "react";
-import CrossChainSlider from "@/components/organisms/homepage/cross-chain-slider";
+import dynamic from "next/dynamic";
+
+const HeroSection = dynamic(
+  () => import("@/components/organisms/homepage/hero-section")
+);
+
+const CrossChainSlider = dynamic(
+  () => import("@/components/organisms/homepage/cross-chain-slider")
+);
+
+const Overview = dynamic(
+  () => import("@/components/organisms/homepage/overview")
+);
+const DappsSection = dynamic(
+  () => import("@/components/organisms/homepage/dapps-section")
+);
+
+const BlogSection = dynamic(
+  () => import("@/components/organisms/homepage/blog-section")
+);
 
 const HomePage = () => {
   return (
