79cfd47e11c7fee5835e9e89c12c9cbf2d46bec7
rajuvemula <rj.vemula81@gmail.com>
2024-09-06 13:35:09 +0530

    seo fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,55 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import dynamic from "next/dynamic";
 import Layout from "@/components/layout";
 
 const HeroSection = dynamic(
-  () => import("@/components/organisms/homepage/hero-section")
+  () => import("@/components/organisms/homepage/hero-section"),
+  {
+    ssr: false,
+    loading: () => <Spinner />
+  }
 );
 
 const CrossChainSlider = dynamic(
-  () => import("@/components/organisms/homepage/cross-chain-slider")
+  () => import("@/components/organisms/homepage/cross-chain-slider"),
+  {
+    ssr: false,
+    loading: () => <Spinner />
+  }
 );
 
 const Overview = dynamic(
-  () => import("@/components/organisms/homepage/overview")
+  () => import("@/components/organisms/homepage/overview"),
+  {
+    ssr: false,
+    loading: () => <Spinner />
+  }
 );
 const DappsSection = dynamic(
-  () => import("@/components/organisms/homepage/dapps-section")
+  () => import("@/components/organisms/homepage/dapps-section"),
+  {
+    ssr: false,
+    loading: () => <Spinner />
+  }
 );
 
 const BlogSection = dynamic(
-  () => import("@/components/organisms/homepage/blog-section")
+  () => import("@/components/organisms/homepage/blog-section"),
+  {
+    ssr: false,
+    loading: () => <Spinner />
+  }
 );
 
 const HomePage = () => {
   return (
     <Layout>
-      <Box>
-        <HeroSection />
-        <CrossChainSlider />
-        <Overview />
-        <DappsSection />
-        <BlogSection />
-      </Box>
+      <HeroSection />
+      <CrossChainSlider />
+      <Overview />
+      <DappsSection />
+      <BlogSection />
     </Layout>
   );
 };
