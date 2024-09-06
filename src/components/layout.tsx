83bfcb1cf6ceca8924d57ef5b531454de134deb3
rajuvemula <rj.vemula81@gmail.com>
2024-09-06 12:23:41 +0530

    seo fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Box, useMediaQuery } from "@chakra-ui/react";
 import GeofenceNotice from "@/components/molecules/geofence-banner";
-// import Header from "@/components/molecules/header";
+import Header from "@/components/molecules/header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Box pos={"fixed"} width="100%" zIndex="sticky">
         <GeofenceNotice />
-        {/*<Header />*/}
+        <Header />
       </Box>
       {children}
       {/*<Footer />*/}
