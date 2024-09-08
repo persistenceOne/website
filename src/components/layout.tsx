5a5415f7d86284e9cb61dfa3e79680d7b48aff67
rajuvemula <rj.vemula81@gmail.com>
2024-09-08 13:35:31 +0530

    update images

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, useMediaQuery } from "@chakra-ui/react";
 import GeofenceNotice from "@/components/molecules/geofence-banner";
 import Footer from "@/components/molecules/footer";
 import Header from "@/components/molecules/header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [isMobile] = useMediaQuery("(max-width: 500px)");
+
   return (
     <>
       <Box pos={"fixed"} width="100%" zIndex="sticky">
-        <GeofenceNotice />
+        {!isMobile ? <GeofenceNotice /> : null}
         <Header />
       </Box>
       {children}
