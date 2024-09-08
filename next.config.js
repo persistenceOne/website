46f1941c44cca80b5087f1fe882533d969cefdba
rajuvemula <rj.vemula81@gmail.com>
2024-09-08 16:31:31 +0530

    cleanup packages

--- next.config.js
+++ next.config.js
@@ -1,25 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  images: {
-    domains: ["raw.githubusercontent.com"],
-    unoptimized: true
-  },
-  async redirects() {
-    return [
-      {
-        source: "/tokensale",
-        destination:
-          "https://blog.pstake.finance/2021/12/13/pstake-tokenomics/",
-        permanent: true
-      }
-    ];
-  },
-  generateBuildId: async () => {
-    return `${new Date().getTime()}`;
-  }
+  reactStrictMode: true,
+  swcMinify: true
 };
 
 module.exports = nextConfig;
