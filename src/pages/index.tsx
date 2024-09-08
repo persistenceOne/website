dc6effd3eefa51e7f0476f20efc3b7e7319ea1c1
rajuvemula <rj.vemula81@gmail.com>
2024-09-08 13:17:16 +0530

    dynamic imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Layout from "@/components/layout";
 import HeroSection from "@/components/organisms/homepage/hero-section";
-import CrossChainSlider from "@/components/organisms/homepage/cross-chain-slider";
 import Overview from "@/components/organisms/homepage/overview";
 import BlogSection from "@/components/organisms/homepage/blog-section";
 import dynamic from "next/dynamic";
+
 const DappsSection = dynamic(
   () => import("@/components/organisms/homepage/dapps-section")
+);
+
+const CrossChainSlider = dynamic(
+  () => import("@/components/organisms/homepage/cross-chain-slider")
 );
 
 const HomePage = () => {
