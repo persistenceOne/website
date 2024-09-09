f1aacd4b457d9f5c899ca8b4f68c1473d7a9a529
rajuvemula <rj.vemula81@gmail.com>
2024-09-09 10:06:57 +0530

    seo fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,13 @@
 import React from "react";
-import dynamic from "next/dynamic";
 import Layout from "@/components/layout";
 import HeroSection from "@/components/organisms/homepage/hero-section";
-import Overview from "@/components/organisms/homepage/overview";
-import BlogSection from "@/components/organisms/homepage/blog-section";
 import CrossChainSlider from "@/components/organisms/homepage/cross-chain-slider";
-
-const DappsSection = dynamic(
-  () => import("@/components/organisms/homepage/dapps-section")
-);
 
 const HomePage = () => {
   return (
     <Layout>
       <HeroSection />
       <CrossChainSlider />
-      <Overview />
-      <DappsSection />
-      <BlogSection />
     </Layout>
   );
 };
