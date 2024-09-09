58d9ed630adf8c3a35b3ae6da0ef8f958038512b
rajuvemula <rj.vemula81@gmail.com>
2024-09-09 10:10:11 +0530

    seo fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Layout from "@/components/layout";
 import HeroSection from "@/components/organisms/homepage/hero-section";
-import CrossChainSlider from "@/components/organisms/homepage/cross-chain-slider";
 
 const HomePage = () => {
   return (
     <Layout>
       <HeroSection />
-      <CrossChainSlider />
     </Layout>
   );
 };
